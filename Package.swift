bf92238821d9e351889312bf1df10387a1de19fe
RN Developer <allen.gzqyl@gmail.com>
2024-03-05 09:48:00 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "iosMath",
+    name: "allenMath",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "iosMath",
-            targets: ["iosMath"]),
+            name: "allenMath",
+            targets: ["allenMath"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,11 +19,11 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "iosMath",
+            name: "allenMath",
             dependencies: [],
             path: "./"),
         .testTarget(
-            name: "iosMathTests",
-            dependencies: ["iosMath"]),
+            name: "allenMathTests",
+            dependencies: ["allenMath"]),
     ]
 )
